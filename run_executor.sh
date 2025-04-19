eb5ed7681d44d6017f5a4c8b0ab194e8e76bed23
tanphap <80197368+tanphap@users.noreply.github.com>
2025-04-19 10:49:49 +0700

    Update run_executor.sh

--- run_executor.sh
+++ run_executor.sh
@@ -15,35 +15,30 @@ export EXECUTOR_MAX_L3_GAS_PRICE=100
 export PRIVATE_KEY_LOCAL=$(cat key.txt)
 export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l2rn,blast-sepolia,unichain-sepolia,monad-testnet'
 export RPC_ENDPOINTS='{
-    "l2rn": ["https://b2n.rpc.caldera.xyz/http"],
-    "arbt": [
-        "https://sepolia-rollup.arbitrum.io/rpc",
-        "https://arbitrum-sepolia.drpc.org"
-    ],
-    "bast": [
-        "https://sepolia.base.org",
-        "https://base-sepolia-rpc.publicnode.com",
-        "https://base-sepolia.drpc.org"
-    ],
-    "blst": [
-        "https://sepolia.blast.io",
-        "https://blast-sepolia.g.alchemy.com/v2/Z1mDP6LGmaLitFKVJgYnbinXdYvkm5Qd",
-        "https://blast-sepolia.g.alchemy.com/v2/0Jho-LFaKw1XJZYGKqYmdxXERGlHWzTD",
-        "https://blast-sepolia.g.alchemy.com/v2/IR99_vEaIeiYgLa-A9kj46CViBq0CBs4"
-    ],
-    "mont": [
-        "https://testnet-rpc.monad.xyz"
-    ],
-    "opst": [
-        "https://sepolia.optimism.io",
-        "https://optimism-sepolia.drpc.org"
-    ],
-    "unit": [
-        "https://rpc.sepolia.unichain.io",
-        "https://unichain-sepolia.api.onfinality.io/public",
-        "https://unichain-sepolia.drpc.org",
-        "https://sepolia.unichain.org"
-    ]
+  "l2rn": ["https://b2n.rpc.caldera.xyz/http"],
+  "arbt": [
+    "https://arbitrum-sepolia.blockpi.network/v1/rpc/public",
+    "https://arbitrum-sepolia.g.alchemy.com/v2/YOUR_API_KEY"
+  ],
+  "bast": [
+    "https://base-sepolia.g.alchemy.com/v2/YOUR_API_KEY",
+    "https://base-sepolia.blockpi.network/v1/rpc/public"
+  ],
+  "blst": [
+    "https://sepolia.blast.io",
+    "https://blast-sepolia.g.alchemy.com/v2/Z1mDP6LGmaLitFKVJgYnbinXdYvkm5Qd",
+    "https://blast-sepolia.g.alchemy.com/v2/0Jho-LFaKw1XJZYGKqYmdxXERGlHWzTD",
+    "https://blast-sepolia.g.alchemy.com/v2/IR99_vEaIeiYgLa-A9kj46CViBq0CBs4"
+  ],
+  "mont": ["https://testnet-rpc.monad.xyz"],
+  "opst": [
+    "https://sepolia.optimism.io",
+    "https://optimism-sepolia.drpc.org"
+  ],
+  "unit": [
+    "https://rpc.testnet.unichain.io",
+    "https://unichain-sepolia.blockpi.network/v1/rpc/public"
+  ]
 }'
 
 export EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=false
