005812cf12c8eb401778eb97a1ddc941cb8cf7f7
tanphap <80197368+tanphap@users.noreply.github.com>
2025-04-19 10:45:09 +0700

    Update run_executor1.sh

--- run_executor.sh
+++ run_executor.sh
@@ -16,13 +16,36 @@ export PRIVATE_KEY_LOCAL=$(cat key.txt)
 export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l2rn,blast-sepolia,unichain-sepolia,monad-testnet'
 export RPC_ENDPOINTS='{
     "l2rn": ["https://b2n.rpc.caldera.xyz/http"],
-    "arbt": ["https://arbitrum-sepolia.drpc.org", "https://sepolia-rollup.arbitrum.io/rpc"],
-    "bast": ["https://base-sepolia-rpc.publicnode.com", "https://base-sepolia.drpc.org"],
-    "blst": ["https://blast-sepolia.g.alchemy.com/v2/Z1mDP6LGmaLitFKVJgYnbinXdYvkm5Qd", "https://blast-sepolia.g.alchemy.com/v2/0Jho-LFaKw1XJZYGKqYmdxXERGlHWzTD", "https://blast-sepolia.g.alchemy.com/v2/IR99_vEaIeiYgLa-A9kj46CViBq0CBs4"],
-    "mont": ["https://testnet-rpc.monad.xyz"],
-    "opst": ["https://sepolia.optimism.io", "https://optimism-sepolia.drpc.org"],
-    "unit": ["https://unichain-sepolia.g.alchemy.com/v2/0Jho-LFaKw1XJZYGKqYmdxXERGlHWzTD", "https://unichain-sepolia.api.onfinality.io/public","https://unichain-sepolia.drpc.org", "https://sepolia.unichain.org"]
+    "arbt": [
+        "https://sepolia-rollup.arbitrum.io/rpc",
+        "https://arbitrum-sepolia.drpc.org"
+    ],
+    "bast": [
+        "https://sepolia.base.org",
+        "https://base-sepolia-rpc.publicnode.com",
+        "https://base-sepolia.drpc.org"
+    ],
+    "blst": [
+        "https://sepolia.blast.io",
+        "https://blast-sepolia.g.alchemy.com/v2/Z1mDP6LGmaLitFKVJgYnbinXdYvkm5Qd",
+        "https://blast-sepolia.g.alchemy.com/v2/0Jho-LFaKw1XJZYGKqYmdxXERGlHWzTD",
+        "https://blast-sepolia.g.alchemy.com/v2/IR99_vEaIeiYgLa-A9kj46CViBq0CBs4"
+    ],
+    "mont": [
+        "https://testnet-rpc.monad.xyz"
+    ],
+    "opst": [
+        "https://sepolia.optimism.io",
+        "https://optimism-sepolia.drpc.org"
+    ],
+    "unit": [
+        "https://rpc.sepolia.unichain.io",
+        "https://unichain-sepolia.api.onfinality.io/public",
+        "https://unichain-sepolia.drpc.org",
+        "https://sepolia.unichain.org"
+    ]
 }'
+
 export EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=false
 
 cd executor/bin
